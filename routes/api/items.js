7460daea04d2ae2cd137a20ef36226f807706357
FranciG <beodo0@gmail.com>
2019-11-27 10:36:18 +0100

    Basic backend crud done.

--- routes/api/items.js
+++ routes/api/items.js
@@ -16,4 +16,27 @@ router.get('/', (req, res) => {
       .then(items => res.json(items));
   });
 
+  // @route   POST api/items
+// @desc    create a item
+// @access  Public
+router.post('/', (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  });
+
+  newItem.save().then(item => res.json(item));
+});
+
+  // @route   DELETE api/items
+// @desc    DELETE a item
+// @access  Public
+router.delete('/:id', (req, res) => {
+//Find the id to be deleted.If id is not found, return 404 error
+Item.findById(req.params.id)
+.then(item => item.remove().then(() => res.json({ success: true })))
+.catch(err => res.status(404).json({ success: false }));
+  });
+
+
+
 module.exports = router;
