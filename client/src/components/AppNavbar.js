8bc6f539187f75cb9a992bb65fe09543b0ca22bd
FranciG <beodo0@gmail.com>
2019-11-27 14:59:18 +0100

    Front end development

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,18 @@
+import React, { Component, Fragment } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container
+} from 'reactstrap';
+
+class AppNavbar extends Component {
+
+ 
+    }
+
+export default AppNavbar;
